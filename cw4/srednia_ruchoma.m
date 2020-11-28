313008eb23fa9b9c1735d1c5940a7ce7e2564901
Grzegorz Kupczyk <kupczykgrzeg@gmail.com>
2020-11-28 20:38:22 +0100

    Z5.4.1

--- cw4/srednia_ruchoma.m
+++ cw4/srednia_ruchoma.m
@@ -4,19 +4,23 @@ function y=srednia_ruchoma(N,x)
 % x jest sygnalem analizowanym
 % y jest wartoscia sredniej sygnalu x za okres N
 
-q=0; % wartosc poczatkowa sumy q
-M=length(x);
-cirbuff=zeros(1,N); % inicjalizacja bufora cyklicznego
-cirbuff_address=1; % inicjalizacja adresu bufora cyklicznego
+q = 0; % wartosc poczatkowa sumy q
+M = length(x);
+cirbuff = zeros(1,N); % inicjalizacja bufora cyklicznego
+cirbuff_address = 1; % inicjalizacja adresu bufora cyklicznego
 for k=1:M
 % 1. pobranie nowej probbki sygnalu (x_new) z wektora x
     x_new = x(k); 
 % 2. obliczenie sumy q
-
+    q = q + x_new - cirbuff(cirbuff_address);
 % 3. zapisanie x_new do bufora cyklicznego (cirbuff) 
-
+    cirbuff(cirbuff_address) = x_new;
 % 4. aktualizacja adresu bufora (cirbuff_address)
-
+    if cirbuff_address == N
+        cirbuff_address = 1;
+    else
+        cirbuff_address = cirbuff_address + 1;
+    end
 % 5. obliczenie wartości sredniej
-    y(k)=q/N;
+    y=q/N;
 end
--- cw4/srednia_test.m
+++ cw4/srednia_test.m
@@ -1,7 +1,7 @@
 % close all;
 clear;
-x_per=[0 1 2 3 4];
-x=[x_per x_per x_per x_per x_per x_per];
+x_per = [0 1 2 3 4];
+x = [x_per x_per x_per x_per x_per x_per];
 y = srednia_ruchoma(5,x);
 Nx = length(x);
 kx = 0:Nx-1;
